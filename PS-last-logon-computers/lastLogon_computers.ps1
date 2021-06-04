16005fd4a1068d72c2941ef7340dbef3bda0ab62
Jeffrey Chin <jeffreychin89@gmail.com>
2021-06-04 14:17:44 -0700

    Provide progression and results in CSV format

--- PS-last-logon-computers/lastLogon_computers.ps1
+++ PS-last-logon-computers/lastLogon_computers.ps1
@@ -1,10 +1,19 @@
 ﻿$Computers = #List of computers here
 
 
-foreach ($Computer in $Computers)
-{
-    echo $Computer":"
-    Get-ADComputer -identity $Computer -Properties * | FT Name, LastLogonDate -Autosize
-}
+Out-File -FilePath C:\temp\last-logon.csv -Encoding UTF8
+$OutHeaders = "Hostname,LastLogonDate"
+Add-Content -Path C:\temp\last-logon.csv -Value $OutHeaders
 
+for ($i = 0; $i -lt $Computers.Count; $i++) {
+
+    $lastLogon = Get-ADComputer -identity $Computers[$i] -Properties * | Select-Object -ExpandProperty LastLogonDate
+    $res = $Computers[$i] + "," + $lastLogon
+    Add-Content -Path C:\temp\last-logon.csv -Value $res
+
+    $P = ((($i + 1) / $Computers.count) * 100)
+    $P = $P -as [int]
+    Write-Progress -Activity "Check in progress" -Status "$P% Complete:" -PercentComplete $P;
+}
+Write-Output "Check completed!"
 Read-Host -Prompt "Press Enter to exit"
